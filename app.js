596efa44cc2ea197d242485808894c62e41b63fe
haochen <haochen2115@gmail.com>
2025-08-02 12:39:15 +0800

    移除node_modules，添加Vercel部署配置

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.get('/', function (req, res) {
   res.send(html)
 })
 
-app.listen(8081, () => { console.log('listening  8081!') })
+app.listen(8081, '0.0.0.0', () => { 
+  console.log('服务器启动成功！')
+  console.log('本地访问: http://localhost:8081')
+  console.log('局域网访问: http://你的电脑IP:8081')
+  console.log('请确保手机和电脑在同一个WiFi网络下')
+})
